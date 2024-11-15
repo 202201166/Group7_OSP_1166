ecb29b9cf6483dc860acf755aae6fad37d12201d
202201166 <202201166@daiict.ac.in>
2024-11-15 13:02:07 +0530

    Update db.js

--- OSP/server/config/db.js
+++ OSP/server/config/db.js
@@ -4,15 +4,14 @@ require("dotenv").config();
 const pool = new Pool({
   // connectionString: process.env.DATABASE_URL,
   //testDB
-  // connectionString: "postgresql://ospdb_user:plPDKR20OhfDsB5wSEBOGilf9RKr7Xea@dpg-crts9d52ng1s73ceertg-a.singapore-postgres.render.com/ospdb",
-  // ssl: true,
-
-  host: "10.100.71.21",
-  port: 5432,
-  user: "202201106",
-  password: "Vraj@6610",
-  database: "202201106",
-  searchPath: ["osp"],
+  connectionString: "postgresql://nishil:vOyQc7nlcyZtLLF6RwgBI5GLuAxSgqGo@dpg-csqur5ij1k6c73c3le5g-a.oregon-postgres.render.com/osp_pxt2",
+  ssl: true,
+  // host: "10.100.71.21",
+  // port: 5432,
+  // user: "202201106",
+  // password: "Vraj@6610",
+  // database: "202201106",
+  // searchPath: ["osp"],
 });
 
 pool
